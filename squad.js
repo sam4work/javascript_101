06368ecc321ef231060823017e3a8c90959b8734
sam4work <sam4work10@gmail.com>
2023-10-11 19:32:31 +0000

    team sqaud exercise

--- squad.js
+++ squad.js
@@ -7,5 +7,32 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
 
+    // get the empty select element on the page
+    const teamSelection = document.getElementById("teams")
+
+    // get the object keys of the squads object variable
+    // and loop over each each
+    Object.keys(squads).map(team => {
+        // create an option element
+        let option = document.createElement('option');
+
+        // set option element value
+        option.value = team.toLowerCase();
+
+        // set inner text for option element
+        option.textContent = team.toUpperCase();
+
+        // add option element as child to the team select element
+        teamSelection.appendChild(
+            option
+        )
+        
+    })
+
+
+
+
+
+
     console.log("Hello World!");
 });
