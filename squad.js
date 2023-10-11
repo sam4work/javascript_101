5e7db4051c41f1c39720db339f2eab3f1268e417
sam4work <sam4work10@gmail.com>
2023-10-11 19:23:23 +0000

    team sqaud exercise

---
+++ squad.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    const squads = {
+        chelsea : ["jackson","essien","drogba"],
+        roma : ["lukaku","dybala","pellegrini"],
+        interMilan : ["thuram","bastoni","barela"],
+    }
+
+
+    console.log("Hello World!");
+});
