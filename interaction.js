53493d3c788849c8e9fbbb6d765eed5d4de9b7dc
sam4work <sam4work10@gmail.com>
2023-10-13 18:30:25 +0000

    interacting with html dom element

---
+++ interaction.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    // document.getElementById(); look for id attribute
+    // document.getElementsByClassName(); look for class attribute
+    // document.getElementsByName(); look for name attribute
+    // document.getElementsByTagName(); look the html tag name
+
+const entireBody = document.getElementsByTagName("body");
+
+console.log(entireBody);
+
+
+});
