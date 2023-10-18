577d079569b0b4555b1b01d887386bb3b0a8e6b0
sam4work <sam4work10@gmail.com>
2023-10-18 19:53:09 +0000

    Blog posts

--- blog.js
+++ blog.js
@@ -3,28 +3,50 @@ import posts from "./blog_post.js";
 
 
 
+
 document.addEventListener("DOMContentLoaded", function () {
 
-    const body = document.getElementsByTagName("body");
-
-    console.log(body);
-
-    for (let index = 0; index < posts.length; index++) {
-
-        const div = document.createElement("div");
-        div.textContent = posts[index].title;
-
-        body[0].appendChild(div);
-        
-    }
+    const postContainer = document.getElementById("post-container");
 
     posts.map(post => {
-        const div = document.createElement("div");
-        div.textContent = post.title;
 
-        body[0].appendChild(div);
+        // Create blog card element
+        const blogCard = document.createElement("div");
+        blogCard.className = "blog-card"; // add class attribute
+
+
+        // Create post image element
+        const img = document.createElement("img");
+        img.alt = post.title; // add alt attribute 
+        img.src = post.image; // add src attribute
+
+        // Create h2 text element
+        const h2 = document.createElement("h2");
+        h2.textContent = post.title; // insert post title as text 
+
+        // Create first p text element
+        const p1 = document.createElement("p");
+        p1.textContent = new Date().toDateString(); // insert date as text 
+        p1.className = "date"; // add class attribute
+
+        // Create first p text element
+        const p2 = document.createElement("p");
+        p2.textContent = post.description; // insert post description as text
+
+        // Create anchor tag element
+        const a = document.createElement("a");
+        a.href = "#"; // add href attribute
+        a.className = "read-more" // add class attribute
+
+        blogCard.appendChild(img); // insert img element into div with blog-card id
+        blogCard.appendChild(h2); // insert h2 element into div with blog-card id
+        blogCard.appendChild(p1); // insert first p element into div with blog-card id
+        blogCard.appendChild(p2); // insert second p element into div with blog-card id
+        blogCard.appendChild(a);// insert anchor element into div with blog-card id
+
+        // add newly created div with blog-card id into div with id of post-container
+        postContainer.appendChild(blogCard);
 
     })
-
 
 });
--- blog_post.js
+++ blog_post.js
@@ -4,70 +4,70 @@ const posts = [
         title: "JavaScript Basics",
         content: "JavaScript is a versatile programming language that allows you to add interactivity...",
         description: "Learn the fundamentals of JavaScript programming.",
-        image: "https://example.com/javascript-basics.jpg"
+        image: "https://loremflickr.com/640/360"
     },
     {
         id: 2,
         title: "Web Design Principles",
         content: "Good web design is crucial for creating an attractive and user-friendly website...",
         description: "Explore the key principles of effective web design.",
-        image: "https://example.com/web-design-principles.jpg"
+        image: "https://loremflickr.com/640/360"
     },
     {
         id: 3,
         title: "Node.js Introduction",
         content: "Node.js is a runtime environment that allows developers to run JavaScript on the server...",
         description: "Get started with Node.js and server-side JavaScript development.",
-        image: "https://example.com/nodejs-introduction.jpg"
+        image: "https://loremflickr.com/640/360"
     },
     {
         id: 4,
         title: "React.js Basics",
         content: "React.js is a popular JavaScript library for building user interfaces...",
         description: "Learn the fundamentals of React.js for building interactive UIs.",
-        image: "https://example.com/reactjs-basics.jpg"
+        image: "https://loremflickr.com/640/360"
     },
     {
         id: 5,
         title: "Responsive Web Design",
         content: "Responsive web design ensures that your website looks and functions well on various devices...",
         description: "Discover the importance of responsive design in modern web development.",
-        image: "https://example.com/responsive-web-design.jpg"
+        image: "https://loremflickr.com/640/360"
     },
     {
         id: 6,
         title: "Diving into CSS Grid",
         content: "CSS Grid is a powerful layout system that simplifies web page layout...",
         description: "Master the art of creating flexible and responsive layouts with CSS Grid.",
-        image: "https://example.com/css-grid.jpg"
+        image: "https://loremflickr.com/640/360"
     },
     {
         id: 7,
         title: "Working with RESTful APIs",
         content: "RESTful APIs are a common way to interact with web services...",
         description: "Learn how to consume and create RESTful APIs in your applications.",
-        image: "https://example.com/restful-apis.jpg"
+        image: "https://loremflickr.com/640/360"
     },
     {
         id: 8,
         title: "Vue.js Essentials",
         content: "Vue.js is a progressive JavaScript framework for building interactive web applications...",
         description: "Explore the core concepts of Vue.js for web development.",
-        image: "https://example.com/vuejs-essentials.jpg"
+        image: "https://loremflickr.com/640/360"
     },
     {
         id: 9,
         title: "Web Security Best Practices",
         content: "Web security is a critical aspect of web development, and it's essential to protect your users...",
         description: "Learn how to secure your web applications with best practices.",
-        image: "https://example.com/web-security.jpg"
+        image: "https://loremflickr.com/640/360"
     },
     {
         id: 10,
         title: "Getting Started with Express.js",
         content: "Express.js is a popular web application framework for Node.js, making it easy to build robust...",
         description: "Start building web applications with Express.js and Node.js.",
-        image: "https://example.com/expressjs-getting-started.jpg"
+        image: "https://loremflickr.com/640/360"
     }
 ];
 
