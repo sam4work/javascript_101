b49f2659a96548186453f9521b1159e1abd1119c
sam4work <sam4work10@gmail.com>
2023-10-27 19:39:29 +0000

    oop login form with exercise

--- oop-classes.js
+++ oop-classes.js
@@ -22,17 +22,22 @@ document.addEventListener("DOMContentLoaded", () => {
                 document.querySelector(`[name=${field}]`).style.border = '1px solid red';
                 break;
             }
+
+            // Username length must be max 20 characters
+            // Username must be alpha numeric
+            // Password length min 8 characters
+            // Password must contain at least 1 capital letter, 1 small letter, 1 number and 1 special character
         }
 
 
         const user = new User();
 
-        if(user.login(userInput.get('username'),userInput.get('password'))){
+        if (user.login(userInput.get('username'), userInput.get('password'))) {
             console.log(userInput);
         }
 
 
-        
+
 
 
     });
