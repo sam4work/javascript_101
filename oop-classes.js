cd3248b689bb26d30c23cc21aa8bda88ff833120
sam4work <sam4work10@gmail.com>
2023-10-27 19:26:54 +0000

    oop login form

--- oop-classes.js
+++ oop-classes.js
@@ -6,12 +6,32 @@ document.addEventListener("DOMContentLoaded", () => {
     // another way to select any DOM Element
     const loginForm = document.querySelector("#loginForm");
 
-    loginForm.addEventListener('submit',(event) => {
+    loginForm.addEventListener('submit', (event) => {
         event.preventDefault();
 
-        const userInput =  new FormData(event.target);
+        const fields = ['username', 'password']
 
-        console.log(userInput);
+        const userInput = new FormData(event.target);
+
+        for (const field of fields) {
+            if (userInput.get(field) === null || userInput.get(field) === "") {
+                const errorField = document.querySelector(`#${field}-error-message`);
+                errorField.textContent = `${field} is required`;
+                errorField.style.color = 'red';
+
+                break;
+            }
+        }
+
+
+        const user = new User();
+
+        if(user.login(userInput.get('username'),userInput.get('password'))){
+            console.log(userInput);
+        }
+
+
+        
 
 
     });
