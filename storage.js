c8f4d6614e376b4d488076b59757f5830e8faadc
sam4work <sam4work10@gmail.com>
2023-11-08 20:06:44 +0000

    Local Storage exercise solved

--- storage.js
+++ storage.js
@@ -1,12 +1,18 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-    const existingTodos = localStorage.getItem('todos');
     // convert string to array of objects
+    const existingTodos = !localStorage.getItem('todos') ? [] :
+        JSON.parse(localStorage.getItem('todos'));
+    ;
+
     // loop through them an append each to ul 
-    $("#todoBox").append(`<li>${existingTodos}</li>`);
+
+    existingTodos.forEach(todo => {
+        $("#todoBox").append(`<li>${todo.title}</li>`);
+    });
 
 
-    $("#todoForm").submit(function(e){
+    $("#todoForm").submit(function (e) {
         e.preventDefault();
 
         // capture form data
@@ -18,10 +24,33 @@ $(document).ready(function(){
 
 
         // get existing todos from local storage
+        const oldTodos = localStorage.getItem('todos');
+
         // combine new todo with existing todos
+        const newTodo = [{
+            title: userInput.get('todo'),
+            completed: false,
+        }]
+
+        let allTodos = [];
+
+        if (!oldTodos) {
+            allTodos = [
+                ...newTodo
+            ]
+        } else {
+            allTodos = [
+                ...JSON.parse(oldTodos),
+                ...newTodo
+            ]
+        }
+
+        console.log(allTodos);
+        console.log(JSON.stringify(allTodos));
+
         // convert array of objects to string
         // Store data in local storage
-        localStorage.setItem('todos',userInput.get('todo'));
+        localStorage.setItem('todos', JSON.stringify(allTodos));
 
 
 
