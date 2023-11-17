8b321d579f0166718b1712057be064f739255efb
sam4work <sam4work10@gmail.com>
2023-11-17 19:00:13 +0000

    Typescript Classes

---
+++ Student.ts
@@ -0,0 +1,28 @@
+
+class Student {
+
+    // public Can use in this and all child class
+    // protected Can use in this class and only in child class
+    // private Can only work in the class
+
+    protected id: string;
+
+    public firstName: string;
+    public lastName: string;
+
+    
+    private _accountNumber : string;
+
+    public get accountNumber() : string {
+        return this._accountNumber;
+    }
+
+    public set accountNumber(v : string) {
+        this._accountNumber = v;
+    }
+    
+
+}
+
+
+const student = new Student();
--- typescript-intro.ts
+++ typescript-intro.ts
@@ -4,8 +4,16 @@ const luckNumber: number = 7;
 
 
 function addTwoNumber(num1: number, num2: number): number {
-    console.log(
-        "Hello"
-    )
-    return num1 * num2;
+
+    console.log("Hello")
+
+    return num1 + num2;
 }
+
+function addNumbers(numbers: number[]) {
+    return numbers.reduce((a, b) => a * b)
+}
+
+addNumbers([
+    10,30,4,80,100
+])
